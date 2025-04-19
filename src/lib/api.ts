711ccb99c784f4dfe1d824855df288114d94c0f2
AlexR <AlexRodwell@users.noreply.github.com>
2025-04-19 19:04:20 +0100

    refactor: remove `query_builder` (unused)

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,22 +1,7 @@
 import { PUBLIC_API_URL } from "$env/static/public";
 
-const query_builder = (parameters: object, prefix = ''): string => {
-    const query = Object.entries(parameters).map(([key, value]) => {
-        const full_key = prefix ? `${prefix}[${key}]` : key;
-
-        if (Array.isArray(value)) {
-            return `${encodeURIComponent(full_key)}=${value.map(encodeURIComponent).join(',')}`;
-        } else if (typeof value === 'object' && value !== null) {
-            return query_builder(value, full_key);
-        }
-        return `${encodeURIComponent(full_key)}=${encodeURIComponent(value)}`;
-    }).join('&');
-
-    return query ? `?${query}` : '';
-}
-
 const query = async (endpoint: string, parameters: object = {}) => {
-    const response = await fetch(`${PUBLIC_API_URL || 'https://api-preview.netrunnerdb.com/api/v3/public'}/${endpoint}`); // }${query_builder(parameters)}
+    const response = await fetch(`${PUBLIC_API_URL || 'https://api-preview.netrunnerdb.com/api/v3/public'}/${endpoint}`);
 
     if (!response.ok) {
         throw new Error(`Error fetching ${endpoint}: ${response.statusText}`);
